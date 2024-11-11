a0c6ffa36c27cb693c9122cbd7eee1629b427626
Xavier <xavierperezfp23@ibf.cat>
2024-11-11 19:14:23 +0100

    todas las imagenes colocadas con sombreado

--- views/home.php
+++ views/home.php
@@ -13,19 +13,19 @@
         </section>
 
         <section class="home-section1">
-            <div>
+            <div id="home-section1-menjarsPreparats">
                 <a href="" class="home-section-links">
                     <h3>MENJARS PREPARATS</h3>
                     <button class="home-buttons">COMPRAR JA</button>
                 </a>
             </div>
-            <div>
+            <div id="home-section1-begudes">
                 <a href="" class="home-section-links">
                     <h3>BEGUDES</h3>
                     <button class="home-buttons">COMPRAR JA</button>
                 </a>
             </div>
-            <div>
+            <div id="home-section1-postres">
                 <a href="" class="home-section-links">
                     <h3>POSTRES</h3>
                     <button class="home-buttons">COMPRAR JA</button>
@@ -34,13 +34,13 @@
         </section>
 
         <section class="home-section2">
-            <div>
+            <div id="home-section1-menjarsDeshidratats">
                 <a href="" class="home-section-links">
                     <h3>MENJARS DESHIDRATATS</h3>
                     <button class="home-buttons">COMPRAR JA</button>
                 </a>
             </div>
-            <div>
+            <div id="home-section1-cerveses">
                 <a href="" class="home-section-links">
                     <h3>CERVESES</h3>
                     <button class="home-buttons">COMPRAR JA</button>
